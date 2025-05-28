a4ec65382f9984659ef2a11a0914bd1ee7fb0442
Prasannadevi R <138461679+prasannadeviramesh@users.noreply.github.com>
2025-05-28 12:51:16 +0530

    Create  Datatypes.java

---
+++ Datatypes.java
@@ -0,0 +1,32 @@
+public class datatypes
+{
+    public static void main(String args[])
+    {
+        boolean flag = true;
+        char ch = 'z';
+        int num = 1234;
+        byte size = 2;
+        short srt = 78;
+        double value = 2.4546778;
+        float temp = 3.8f;
+        long val = 1888889;
+        System.out.println("boolean: " + flag);
+        System.out.println("char: " + ch);
+        System.out.println("integer: " + num);
+        System.out.println("byte: " + size);
+        System.out.println("short: " + srt);
+        System.out.println("float: " + value);
+        System.out.println("double: " + temp);
+        System.out.println("long: " + val);
+    }
+}
+/*Output
+
+boolean: true
+char: z
+integer: 1234
+byte: 2
+short: 78
+float: 2.4546778
+double: 3.8
+long: 1888889 */
